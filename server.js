99c7b74e97da217d84df4ddc6dcdc233347dde35
TechGitMaster <52535645+TechGitMaster@users.noreply.github.com>
2022-07-05 15:50:17 +0800

    first

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const body = require('body-parser');
+
+const app = express();
+
+
+app.use(body.urlencoded({extended: true}));
+app.use(body.json());
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/abpadilla'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/abpadilla/'}),
+);
+
+var server = require('http').createServer(app);
+
+// Start the app by listening on the default Heroku port
+server.listen(process.env.PORT || 8080);
