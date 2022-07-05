77fe1144974f08d008e55efbbb3ec60a34ae5295
TechGitMaster <52535645+TechGitMaster@users.noreply.github.com>
2022-07-05 16:28:27 +0800

    rechange

---
+++ nodejs/trys.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/jwt', (req, res) => {
+    res.json({'datas': "ahahahaha"});
+});
+
+module.exports = router;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';  
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'abpadilla';
+
+  https!: any;
+  subs!: Subscription;
+
+  constructor(private http: HttpClient){
+    this.https = http;
+  } 
+
+  ngOnInit(): void {
+    this.subs = this.https.get('./jwt').subscribe((data: any) => {
+      console.log(data+" asdasdsad");
+    });
+
+    this.subs.unsubscribe();
+  }
 }
