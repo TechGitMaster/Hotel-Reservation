3178cf7cf4342dbe6a7da54578d9f0bf00f75256
TechGitMaster <52535645+TechGitMaster@users.noreply.github.com>
2022-08-06 14:48:12 +0800

    updated

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ app.use(body.urlencoded({extended: true}));
 app.use(body.json());
 app.use('/', require('./server_controller'));
 
-//Protection____________________________________________
-
-
 app.use((req, res, next) => {
     res.setHeader('Access-Origin-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
